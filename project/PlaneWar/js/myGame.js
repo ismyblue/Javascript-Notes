5dd9f80d81b91255a2e29e31ea71bd326e934e45
Bruce <ismyblue@163.com>
2017-10-30 12:10:49 +0800

    continue

--- project/PlaneWar/js/myGame.js
+++ project/PlaneWar/js/myGame.js
@@ -13,36 +13,36 @@ function CSpirit() {
 CSpirit.prototype.max_x_coor = 1000;    //最大x坐标
 CSpirit.prototype.max_y_coor = 1000;    //最大y坐标
 
-
+//初始化，设置id,class,父容器id,初始宽度，初始高度
 CSpirit.prototype.init = function(sId, sClass, sPreId, sWidth, sHeight){
     this.id = sId;     //css id
     this.class = sClass;  //css class
+    this.preId = sPreId;
     if(sWidth != null)
         this.width = sWidth;
     if(sHeight != null)
         this.height = sHeight;
-    this.preId = sPreId;
 }
 
-CSpirit.prototype.show = function(sWidth, sHeight){
+//在父容器内显示精灵元素，可以传参数给定显示宽度高度
+CSpirit.prototype.show = function(sPreId, sWidth, sHeight){
+    if(sPreId != null)
+        this.preId = sPreId;
+    if(sWidth != null)
+        this.width = sWidth;
+    if(sHeight != null)
+        this.height = sHeight;
     var div = document.createElement("div");
     div.id = this.id;
-    div.className = this.class;
-    if(sWidth == null)
-        div.style.width = this.width;
-    else
-        div.style.width = sWidth;
-    if(sHeight == null)
-        div.style.height = this.height;
-    else
-        div.style.height = sHeight;
+    div.style.width = this.width;
+    div.style.height = this.height;
     div.style.left = this.x_coor;
     div.style.top = this.y_coor;
     var pre = document.getElementById(this.preId);
     pre.appendChild(div);
 }
 
-
+//移动精灵位置，传入参数，x和y的坐标，坐标原点在左上角
 CSpirit.prototype.move = function(sX_coor, sY_coor){
     var myself = document.getElementById(this.id);
     this.x_coor = sX_coor;
@@ -51,6 +51,7 @@ CSpirit.prototype.move = function(sX_coor, sY_coor){
     myself.style.top = this.y_coor;
 }
 
+//在dom销毁此元素
 CSpirit.prototype.destory = function(){
     var myself = document.getElementById(this.id);
     myself.parentNode.removeChild(myself);
@@ -60,7 +61,7 @@ CSpirit.prototype.destory = function(){
 
 
 
-//----------CContainer Class Definition Begin--------//
+//----------CContainer Class Definition Begin--------//容器类
 function CContainer(){
     CSpirit.call(this);
     CContainer._defi_CContainer = true;
@@ -74,20 +75,25 @@ con.init("gamediv","game-room","main");
 con.show();
 /**/
 
-//----------CInformation Class Definition Begin--------//
+
+//----------CInformation Class Definition Begin--------//消息类
 function CInformation(){
     CSpirit.call(this);
     this.title = null;
     this.content = null;
 }
 CInformation.prototype = new CSpirit();
+//设置消息，为这一条消息设置标题和内容
 CInformation.prototype.setInformation = function(sTitle, sContent){
     this.title = sTitle;
     this.content = sContent;
 }
-CInformation.prototype.showBody = CInformation.prototype.show;
-CInformation.prototype.show = function (sWidth, sHeight) {
-    this.showBody(sWidth, sHeight);
+
+CInformation.prototype.showBody = CSpirit.prototype.show;
+
+//显示一条消息，在preid的容器内显示宽为sWidth,sHeight的消息
+CInformation.prototype.show = function(sPreId, sWidth, sHeight){
+    this.showBody(sPreId, sWidth, sHeight);
     var myself = document.getElementById(this.id);
     var label = document.createElement("label");
     label.className = "text-primary";
@@ -98,24 +104,138 @@ CInformation.prototype.show = function (sWidth, sHeight) {
     myself.appendChild(label);
     myself.appendChild(p);
 }
-
-
 //----------CInformation Class Definition End--------//
 
-/*var s = new CInformation();//CInformation();
-s.init("info","information","gamediv");
-s.setInformation("title","content");
-s.show();*/
+
+
+//----------CInformationGroup Class Definition Begin--------//消息组类
+function CInformationGroup(){
+    CContainer.call(this);
+    this.infos = new Array();
+}
+
+CInformationGroup.prototype = new CContainer();
+
+//在消息组内增加一条消息，消息标题，内容，id,class,高度和宽度
+CInformationGroup.prototype.addInformation = function(sTitle, sContent,sId, sClass, sWidth, sHeight){
+    var len = this.infos.length;
+    this.infos[len] = new CInformation();
+    this.infos[len].init(sId, sClass, this.id, sWidth, sHeight);
+    this.infos[len].title = sTitle;
+    this.infos[len].content = sContent;
+}
+
+//更名父类的show函数为showBody
+CInformationGroup.prototype.showBody = CContainer.prototype.show;
+
+//显示消息组，sNum代表一列显示多少条消息,不传sWidth和sHeight默认消息组是100%
+//一条消息包含titile和content，消息组有多条消息
+CInformationGroup.prototype.show = function (iNum, sPreId, sWidth, sHeight) {
+    this.showBody(sPreId, sWidth, sHeight);
+    var nowWidth = this.width;
+    var nowHeight = this.height;
+    var num = 1;
+    if(sNum != null)
+        num = sNum;
+    nowWidth =  (parseInt(nowWidth)/num).toString() + "%";
+    var t = this.infos.length/num;
+    if(t > parseInt(t))
+        t = parseInt(t) + 1;
+    else
+        t = parseInt(t);
+    nowHeight = (nowHeight/t).toString() + "%";
+    var myself = document.getElementById(this.id);
+    for(var i = 0;i < this.infos.length;i++) {
+        this.infos[i].show(this.id, nowWidth, nowHeight);
+    }
+}
+//----------CInformationGroup Class Definition End--------//
+
+
+
+
+//------------CButton Class Definition Begin--------------//
+function CButton(){
+    CSpirit.call(this);
+    this.value = null;
+    this.functionName = null;
+}
+CButton.prototype = new CSpirit();
+//设置一按钮，按钮名字，按钮的函数名
+CButton.prototype.setButton = function(sValue, sFunctionName) {
+    this.value = sValue;
+    this.functionName = sFunctionName;
+}
+
+CButton.prototype.init = function(){
+    
+}
+
+//CButton.prototype.showBody = CSpirit.prototype.show;
+CButton.prototype.show = function(sPreId, sWidth, sHeight){
+
+}
+
+//------------CButton Class Definition Begin--------------//
+
+
+
+
+//------------CButtonGroup Class Definition Begin--------------//
+//按钮组类
+function CButtonGroup(){
+    CSpirit.call(this);
+    this.buttons = new Array();
+}
+CButtonGroup.prototype = new CSpirit();
+
+//在消息组内增加一条消息，消息标题，内容，id,class,高度和宽度
+CButtonGroup.prototype.addButton = function(sValue, sFunctionName, sId, sClass, sWidth, sHeight){
+    var len = this.buttons.length;
+    this.buttons[len] = new CButton();
+    this.buttons[len].init(sId, sClass, this.id, sWidth, sHeight);
+    this.buttons[len].value = sValue;
+    this.buttons[len].functionName = sFunctionName;
+}
+
+//更名父类的show函数为showBody
+CButtonGroup.prototype.showBody = CSpirit.prototype.show;
+
+//在按钮组显示消息，sNum代表一列显示多少个按钮,不传sWidth和sHeight默认是100%
+CButtonGroup.prototype.show = function (iNum) {
+    this.showBody(sWidth, sHeight);
+    var num = 1;
+    if(sNum != null)
+        num = iNum;
+    var myself = document.getElementById(this.id);
+    for(var i = 0;i < this.infos.length;i++) {
+        var div = document.createElement("div");
+        if(this.infos[i].class != null)
+            div.className = this.infos[i].class;
+        div.style.width = (100/iNum).toString() + "%";
+        var label = document.createElement("label");
+        label.className = "text-primary";
+        label.innerHTML = this.infos[i].title;
+        var p = document.createElement("p");
+        p.className = "text-danger";
+        p.innerHTML = this.infos[i].content;
+        div.appendChild(label);
+        div.appendChild(p);
+        myself.appendChild(div);
+    }
+}
+//------------CButtonGroup Class Definition End--------------//
 
 
 
 //----------CPanel Class Definition Begin--------//
 function CPannel(){
-    CSpirit.call(this);
-    this.infos = new Array();
+    CContainer.call(this);
+    this.infomationgroup = null;
+    this.buttongroup = null;
 }
 
-CPannel.prototype = new CSpirit();
+CPannel.prototype = new CContainer();
 
 CPannel.prototype.addInfo = function(sTitle, sContent){
     var len = this.infos.length;
@@ -142,3 +262,4 @@ panel.init("panel", "panel-info", "gamediv", "100%", "10%");
 panel.addInfo("score","1205");
 panel.addInfo("blood","18");
 panel.show();
+
